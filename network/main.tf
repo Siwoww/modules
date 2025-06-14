11bd29589070dc8447d38ef22596126665da38b6
Siwoww <simone1ft93@gmail.com>
2025-06-14 22:54:36 +0200

    commit

---
+++ network/main.tf
@@ -0,0 +1,40 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "main" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.subnet_cidr
+  map_public_ip_on_launch = true #aggiunto da gpt
+}
+
+resource "aws_internet_gateway" "main_gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "main_route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = var.route_cidr_block
+    gateway_id = aws_internet_gateway.main_gw.id
+  }
+
+  /*route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  }*/
+
+  tags = {
+    Name = "main_route"
+  }
+}
+
+resource "aws_route_table_association" "main_assoc" {
+  subnet_id = aws_subnet.main.id
+  route_table_id = aws_route_table.main_route.id
+}
