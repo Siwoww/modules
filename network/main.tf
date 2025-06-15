2c2cecb71b4479374ab8c5e7f879ebae11b750bf
Siwoww <simone1ft93@gmail.com>
2025-06-15 14:04:46 +0200

    commit

--- network/main.tf
+++ network/main.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id = aws_vpc.main.id
   cidr_block = var.subnet_cidr
-  #map_public_ip_on_launch = true #aggiunto da gpt
+  map_public_ip_on_launch = true #aggiunto da gpt
 }
 
 resource "aws_internet_gateway" "main_gw" {
