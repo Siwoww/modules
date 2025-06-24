004e7a2616bb59eb6249cee0f62622737ccd692e
Siwoww <simone1ft93@gmail.com>
2025-06-24 13:14:49 +0200

    commit

--- s3-bucket-remote-state/outputs.tf
+++ s3-bucket-remote-state/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket" {
-  value = aws_s3_bucket.main.bucket
+  value = aws_s3_bucket.remote_statefile_s3_bucket.bucket
 }
 
 output "region" {
-  value = aws_s3_bucket.main.region
+  value = aws_s3_bucket.remote_statefile_s3_bucket.region
 }
